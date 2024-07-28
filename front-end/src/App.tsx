efc7429d3406a7d32c7f028ccdb70841a39bdc50
abulisu12 <abulfazlsoultnii12@gmail.com>
2024-07-28 16:33:14 +0330

    initial commit
    . install project dependency

---
+++ front-end/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>test form...</h1>
+}
+
+export default App
---
+++ front-end/src/main.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.tsx'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />,
+)
