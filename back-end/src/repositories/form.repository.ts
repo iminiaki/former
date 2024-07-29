ea4fb762de9b030a1063ef4e1c5119535e77df47
Parnia Eshaghi <parniaeshaghi@gmail.com>
2024-07-29 11:50:10 +0330

    added user & form repos

---
+++ back-end/src/repositories/form.repository.ts
@@ -0,0 +1,69 @@
+import { Form } from "../models/form.model";
+import { FormElement } from "../models/formElement.model";
+import { SubmittedForm } from "../models/submittedForm.model";
+
+export interface CreateForm {
+    name: string;
+    description: string;
+    elements: FormElement[];
+}
+
+export interface IFormRepository {
+    createForm(form: CreateForm): Form;
+    readForm(formId: number): Form | undefined;
+    updateForm(form: Form): boolean;
+    deleteForm(form: Form): boolean;
+    getAllForms(): Form[];
+}
+
+export class FormRepository implements IFormRepository {
+    private formRepo: Form[];
+
+    constructor() {
+        this.formRepo = [];
+    }
+
+    private genId(): number {
+        return this.formRepo.length + 1;
+    }
+
+    public createForm(form: CreateForm): Form {
+        const submittedForms: SubmittedForm[] = [];
+        const newForm: Form = { ...form, id: this.genId(), submittedForms: submittedForms };
+        this.formRepo.push(newForm);
+        return newForm;
+    };
+
+    public readForm(formId: number): Form | undefined {
+        return this.formRepo.find((x) => x.id == formId);
+    };
+
+    public updateForm(form: Form): boolean {
+        try {
+            const readForm = this.readForm(form.id);
+            if (readForm) {
+                readForm.name = form.name;
+                readForm.description = form.description;
+                readForm.elements = form.elements;
+            }
+            return true;
+        } catch (e) {
+            return false;
+        }
+    };
+
+    public deleteForm(form: Form): boolean {
+        try {
+            const index = this.formRepo.indexOf(form);
+            if (index >= 0) {
+                this.formRepo.splice(index, 1);
+            }
+            return true;
+        } catch (e) {
+            return false;
+        }
+    };
+    public getAllForms(): Form[] {
+        return this.formRepo;
+    };
+}
