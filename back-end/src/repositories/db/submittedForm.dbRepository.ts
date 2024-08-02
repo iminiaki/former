4b03ef6cb6738b3ab13db05a48e59bdf4914fc09
Mahdi Fardi <mrmahdifardi@gmail.com>
2024-08-02 14:13:40 +0330

    SubmittedFormDbRepository fix

--- back-end/src/repositories/db/submittedForm.dbRepository.ts
+++ back-end/src/repositories/db/submittedForm.dbRepository.ts
@@ -1,6 +1,9 @@
 import { DataSource, Repository } from "typeorm";
 import { IUserRepository } from "../user.repository";
-import { CreateSubmittedForm, ISubmittedFormRepository } from "../submittedForm.repository";
+import {
+    CreateSubmittedForm,
+    ISubmittedFormRepository,
+} from "../submittedForm.repository";
 import { SubmittedFormEntity } from "../../entities/submittedForm.entity";
 import { SubmittedForm } from "../../models/submittedForm.model";
 
@@ -11,32 +14,51 @@ export class SubmittedFormDbRepository implements ISubmittedFormRepository {
             appDataSource.getRepository(SubmittedFormEntity);
     }
 
-    public async createSubmittedForm(submittedForm: CreateSubmittedForm): Promise<SubmittedFormEntity> {
+    public createSubmittedForm(
+        submittedForm: CreateSubmittedForm
+    ): SubmittedForm {
         const newSubmittedForm = this.submittedFormRepo.create(submittedForm);
-        await this.submittedFormRepo.save(newSubmittedForm);
-        return newSubmittedForm;
+        this.submittedFormRepo.save(newSubmittedForm);
+        return {
+            id: Number(newSubmittedForm.id),
+            email: newSubmittedForm.email,
+            data: newSubmittedForm.data,
+        };
     }
 
-    public async readSubmittedFormById(submittedFormId: number): Promise<SubmittedFormEntity | null> {
+    public async readSubmittedFormById(
+        submittedFormId: number
+    ): Promise<SubmittedFormEntity | null> {
         return this.submittedFormRepo.findOneBy({ id: submittedFormId });
     }
 
-    public async readSubmittedFormByEmail(submittedFormEmail: string): Promise<SubmittedFormEntity | null> {
+    public async readSubmittedFormByEmail(
+        submittedFormEmail: string
+    ): Promise<SubmittedFormEntity | null> {
         return this.submittedFormRepo.findOneBy({ email: submittedFormEmail });
     }
 
-    public async updateSubmittedForm(submittedForm: SubmittedForm): Promise<boolean> {
+    public async updateSubmittedForm(
+        submittedForm: SubmittedForm
+    ): Promise<boolean> {
         try {
-            const result = await this.submittedFormRepo.update(submittedForm.id, submittedForm);
+            const result = await this.submittedFormRepo.update(
+                submittedForm.id,
+                submittedForm
+            );
             return result.affected !== 0;
         } catch (e) {
             return false;
         }
     }
 
-    public async deleteSubmittedForm(submittedForm: SubmittedForm): Promise<boolean> {
+    public async deleteSubmittedForm(
+        submittedForm: SubmittedForm
+    ): Promise<boolean> {
         try {
-            const result = await this.submittedFormRepo.delete(submittedForm.id);
+            const result = await this.submittedFormRepo.delete(
+                submittedForm.id
+            );
             return result.affected !== 0;
         } catch (e) {
             return false;
